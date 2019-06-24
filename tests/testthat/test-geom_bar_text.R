85462e6d5a477b492a30274a477ab1283c83d432
David Wilkins <david@wilkox.org>
2019-06-24 14:08:22 +1000

    Add tests for geom_bar_text()

---
+++ tests/testthat/test-geom_bar_text.R
@@ -0,0 +1,24 @@
+context("basic bar plots")
+
+test_that("simple bar plots and options do not produce errors", {
+  expect_silent( {
+    p <- ggplot2::ggplot(altitudes, ggplot2::aes(x = craft, y = altitude, label = altitude)) +
+      geom_col() +
+      geom_bar_text()
+    print(p)
+
+    p <- ggplot2::ggplot(coffees, ggplot2::aes(x = coffee, y = proportion, label = ingredient,
+                        fill = ingredient)) +
+      geom_col(position = "stack") +
+      geom_bar_text(position = "stack", grow = TRUE, reflow = TRUE)
+    print(p)
+
+    p <- ggplot2::ggplot(coffees, ggplot2::aes(x = coffee, y = proportion, label = ingredient,
+                        fill = ingredient)) +
+      geom_col(position = "dodge") +
+      geom_bar_text(position = "dodge", grow = TRUE, reflow = TRUE, 
+                    place = "left") +
+      coord_flip()
+    print(p)
+  } )
+} )
