e33e6e4b524f3cb117a9b2d5a374286dd7e24bee
David Wilkins <david@wilkox.org>
2023-08-22 09:51:47 +0930

    Fix ggfittext-package documentation https://github.com/r-lib/roxygen2/issues/1491

--- R/ggfittext.R
+++
@@ -1,7 +0,0 @@
-#' 'ggfittext': provides a 'ggplot2' geom to fit text inside a box.
-#'
-#' This package provides only one function, `geom_fit_text()`.
-#'
-#' @docType package
-#' @name ggfittext
-NULL
